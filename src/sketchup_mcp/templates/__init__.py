a016fa6421dd9fc30a4bd936d8cfefab134acb9e
gaius-codius <maher@najemft.com>
2026-01-21 05:49:23 +0000

    feat: add box template with lid support and template registry

--- src/sketchup_mcp/templates/__init__.py
+++ src/sketchup_mcp/templates/__init__.py
@@ -2,5 +2,19 @@
 
 from .base import BaseTemplate, TemplateResult, LumberPiece
 from .bookshelf import BookshelfTemplate
+from .box import BoxTemplate
 
-__all__ = ["BaseTemplate", "TemplateResult", "LumberPiece", "BookshelfTemplate"]
+# Template registry for discovery
+TEMPLATES = {
+    "bookshelf": BookshelfTemplate,
+    "box": BoxTemplate,
+}
+
+__all__ = [
+    "BaseTemplate",
+    "TemplateResult",
+    "LumberPiece",
+    "BookshelfTemplate",
+    "BoxTemplate",
+    "TEMPLATES",
+]
